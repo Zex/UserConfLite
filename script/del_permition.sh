4eb34c32b5a94f7a7051c79d035844e16a1bf98a
Zex <top_zlynch@yahoo.com>
2014-11-05 01:25:58 -0800

    add selftest script

--- script/del_permition.sh
+++ script/del_permition.sh
@@ -3,3 +3,6 @@
 RE_TEXT='^.*\(\(default\|README\|LICENSE\|MakeFile\)\|\(\.\(mk\|sql\|h\|cpp\|png\|bmp\|jpg\|php\|html\|conf\|Sample\|xls\|doc\|pdf\|css\)\)\)$'
 
 find . -type f -iregex $RE_TEXT -exec echo {} \; -exec chmod a-x {} \;
+
+rm UserConf.db
+ln -s UserConf.db3x UserConf.db
