d10e5e6d0c8e4857d0d6fc4df0a29c83a44cc511
Zex <top_zlynch@yahoo.com>
2014-11-05 08:47:41 +0800

    Create basic.mk

---
+++ makefiles/basic.mk
@@ -0,0 +1,38 @@
+# makefiles/basic.mk
+
+ifndef PROJECT
+$(error PROJECT not defined)
+endif
+
+ifndef VERSION
+$(error VERSION not defined)
+endif
+
+ifndef DESCRIPTION
+$(error DESCRIPTION not defined)
+endif
+
+all: $(VERSION_HEADER)
+
+$(BUILD):
+	@echo
+	@echo "===Generating $@===>"
+	$(MKDIR) $@
+
+$(VERSION_HEADER): $(BUILD)
+	@echo
+	@echo "===Generating $@===>"
+	@touch $@
+	@echo "/* Automated generated file - `date -u` */" > $@
+	@echo "#pragma once" >> $@
+	@echo >> $@
+	@echo "#define PROJECT \"$(PROJECT)\"" >> $@
+	@echo "#define DESCRIPTION \"$(DESCRIPTION)\"" >> $@
+	@echo "#define VERSION \"$(VERSION)\"" >> $@
+	@echo "#define REVISION \"$(REVISION)\"" >> $@
+	@echo "#define LAST_BUILD_TIME \"`date -u`\"" >> $@
+	@echo >> $@
+	@echo 
+
+
+
