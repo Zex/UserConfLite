7913c54132523b603721343295148b41b68bd454
Zex <top_zlynch@yahoo.com>
2014-11-07 04:25:09 -0800

    create data in user config from sys

--- dbs/init_db.sql
+++ dbs/init_db.sql
@@ -1,30 +1,17 @@
 /*
+ * init_db.sql
+ *
+ * SQL sripts to initiate configure for 3D/4D
+ *
+ * Author: Zex <top_zlynch@yahoo.com>
+ *
  * ValueType:
  * 0 => double
  * 1 => int
  * 2 => string
  *
-create table UserConf (
-Key text primary key not null,
-Value text not null,
-ValueType integer not null
-);
-
-insert into UserConf values ("Prepare.SwingAngle", "60.0", 0);
-insert into UserConf values ("Prepare.MovingLength", "11.0", 0);
-insert into UserConf values ("Prepare.QualityIndex", "2.0", 0);
-insert into UserConf values ("View.Vision", "0.0", 0);
-insert into UserConf values ("View.QRotate", "0.0", 0);
-insert into UserConf values ("View.RotationX", "270.0", 0);
-insert into UserConf values ("View.RotationY", "0.0", 0);
-insert into UserConf values ("View.RotationZ", "270.0", 0);
-insert into UserConf values ("View.Chroma", "2.0", 0);
-insert into UserConf values ("View.Graymap", "3.0", 0);
-insert into UserConf values ("View.Zoom", "1.0", 0);
-insert into UserConf values ("View.Render", "3.0", 0);
-insert into UserConf values ("Slice.Shift", "0.0", 0);
-insert into UserConf values ("Prepare.PreferedScan", "3D", 2);
-
+ * Sample usage: sqlite3 ConfLite < init_db.sql
+ */
 create table SysConf (
     Key text primary key not null,
     DefaultValue text not null,
@@ -34,18 +21,46 @@ create table SysConf (
     Unit text
 );
 
-insert into SysConf values ("Prepare.SwingAngle", 60.0, 2.0, 30.0, 1.0, "");
-insert into SysConf values ("Prepare.MovingLength", 11.0, 2.0, 30.0, 1.0, "cm");
-insert into SysConf values ("Prepare.QualityIndex", 2.0, 2.0, 30.0, 1.0, "");
-insert into SysConf values ("View.Vision", 0.0, 2.0, 30.0, 1.0, "");
-insert into SysConf values ("View.QRotate", 0.0, 2.0, 30.0, 1.0, "");
-insert into SysConf values ("View.RotationX", 270.0, 2.0, 30.0, 1.0, "");
-insert into SysConf values ("View.RotationY", 0.0, 2.0, 30.0, 1.0, "");
-insert into SysConf values ("View.RotationZ", 270.0, 2.0, 30.0, 1.0, "");
-insert into SysConf values ("View.Chroma", 2.0, 2.0, 30.0, 1.0, "");
-insert into SysConf values ("View.Graymap", 3.0, 2.0, 30.0, 1.0, "");
-insert into SysConf values ("View.Zoom", 1.0, 2.0, 30.0, 1.0, "");
-insert into SysConf values ("View.Render", 3.0, 2.0, 30.0, 1.0, "");
-insert into SysConf values ("Slice.Shift", 0.0, 2.0, 30.0, 1.0, "");
- */
+                            /* Key,                 DefVal,  Step,   Upper,   Lower,  Unit */
+insert into SysConf values ("Prepare.SwingAngle",   "70.0",  "10.0", "70.0",  "20.0", "");
+insert into SysConf values ("Prepare.MovingLength", "11.0",  "1.0",  "7.0",   "99.0", "cm");
+insert into SysConf values ("Prepare.QualityIndex", "2.0",   "1.0",  "2.0",   "1.0",  "");
+insert into SysConf values ("View.Vision",          "0.0",   "1.0",  "2.0",   "1.0",  "");
+insert into SysConf values ("View.QRotate",         "0.0",   "90.0", "270.0", "0.0",  "");
+insert into SysConf values ("View.RotationX",       "270.0", "5.0",  "360.0", "0.0",  "");
+insert into SysConf values ("View.RotationY",       "0.0",   "5.0",  "360.0", "0.0",  "");
+insert into SysConf values ("View.RotationZ",       "270.0", "5.0",  "360.0", "0.0",  "");
+insert into SysConf values ("View.Chroma",          "2.0",   "1.0",  "7.0",   "0.0",  "");
+insert into SysConf values ("View.Graymap",         "6.0",   "1.0",  "6.0",   "0.0",  "");
+insert into SysConf values ("View.Zoom",            "1.0",   "0.4",  "2.2",   "0.6",  "");
+insert into SysConf values ("View.Render",          "3.0",   "1.0",  "4.0",   "0.0",  "");
+insert into SysConf values ("Slice.Shift",          "0.0",   "0.1",  "1.0",   "0.0",  "");
 
+create table UserConf (
+    Key text primary key not null,
+    Value text not null,
+    ValueType integer not null
+);
+
+insert into UserConf (Key, Value, ValueType) 
+    select Key, DefaultValue, 0 from SysConf;/* where Key = "Prepare.SwingAngle"; */
+
+insert into UserConf values ("Prepare.PreferedScan", "3D",    2);
+
+                            /* Key,                  Value,   ValueType */
+/*
+insert into UserConf values ("Prepare.SwingAngle",   "60.0",  0);
+insert into UserConf values ("Prepare.MovingLength", "11.0",  0);
+insert into UserConf values ("Prepare.QualityIndex", "2.0",   0);
+insert into UserConf values ("View.Vision",          "0.0",   0);
+insert into UserConf values ("View.QRotate",         "0.0",   0);
+insert into UserConf values ("View.RotationX",       "270.0", 0);
+insert into UserConf values ("View.RotationY",       "0.0",   0);
+insert into UserConf values ("View.RotationZ",       "270.0", 0);
+insert into UserConf values ("View.Chroma",          "2.0",   0);
+insert into UserConf values ("View.Graymap",         "3.0",   0);
+insert into UserConf values ("View.Zoom",            "1.0",   0);
+insert into UserConf values ("View.Render",          "3.0",   0);
+insert into UserConf values ("Slice.Shift",          "0.0",   0);
+insert into UserConf values ("Prepare.PreferedScan", "3D",    2);
+*/
