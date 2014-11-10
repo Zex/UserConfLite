fe84dc13db11522a23ed0b362473efecce50ed11
Zex <top_zlynch@yahoo.com>
2014-11-10 05:19:13 -0800

    update dbs

--- dbs/update_uc.sql
+++ dbs/update_uc.sql
@@ -1,7 +1,12 @@
 begin transaction;
-
+/*
 insert into UserConf (Key, Value, ValueType) 
     select Key, DefaultValue, 0 from SysConf where key like "Debug.%" or Key="View.Threshold";
+*/
+
+/*
+update UserConf set Key="Prepare.Distance" where Key="Prepare.MovingLength";
+*/
 
 select * from UserConf;
 
