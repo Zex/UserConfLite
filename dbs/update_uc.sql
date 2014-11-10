919e6cda2964a0234721afcdc3633a7d38a272a3
Zex <top_zlynch@yahoo.com>
2014-11-10 23:59:35 +0800

    update dbs sql

--- dbs/update_uc.sql
+++ dbs/update_uc.sql
@@ -7,10 +7,13 @@ insert into UserConf (Key, Value, ValueType)
 /*
 update UserConf set Key="Prepare.Distance" where Key="Prepare.MovingLength";
 */
-
+/*
+update UserConf set Value="100.0" where Key="View.Threshold";
+update UserConf set Value="111.0" where Key="Prepare.Distance";
+*/
 update UserConf
-set Value = (select SysConf.DefaultValue from SysConf where UserConf.Key=SysConf.Key)
-where exists (select * from SysConf where UserConf.Key=SysConf.Key);
+set Value = (select SysConf.DefaultValue from SysConf where UserConf.Key=SysConf.Key and SysConf.Key like "View.%")
+where exists (select * from SysConf where UserConf.Key=SysConf.Key and SysConf.Key like "View.%");
 
 select Key, '-',  Value, '-', ValueType from UserConf;
 
