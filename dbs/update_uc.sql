58704ec56080134461fef25a6b982982b4f7773e
Zex <top_zlynch@yahoo.com>
2014-11-11 04:24:28 -0800

    update config.db

--- dbs/update_uc.sql
+++ dbs/update_uc.sql
@@ -10,10 +10,11 @@ update UserConf set Key="Prepare.Distance" where Key="Prepare.MovingLength";
 /*
 update UserConf set Value="100.0" where Key="View.Threshold";
 update UserConf set Value="111.0" where Key="Prepare.Distance";
-*/
+
 update UserConf
 set Value = (select SysConf.DefaultValue from SysConf where UserConf.Key=SysConf.Key and SysConf.Key like "View.%")
 where exists (select * from SysConf where UserConf.Key=SysConf.Key and SysConf.Key like "View.%");
+*/
 
 select Key, '-',  Value, '-', ValueType from UserConf;
 
