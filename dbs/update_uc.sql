5d333e208ccf612a876c5fa2e82c78be75d68049
Zex <top_zlynch@yahoo.com>
2014-11-15 23:29:46 +0800

    update update_uc.sql

--- dbs/update_uc.sql
+++ dbs/update_uc.sql
@@ -16,11 +16,12 @@ update UserConf set Value="111.0" where Key="Prepare.Distance";
 update UserConf
 set Value = (select SysConf.DefaultValue from SysConf where UserConf.Key=SysConf.Key and SysConf.Key like "View.%")
 where exists (select * from SysConf where UserConf.Key=SysConf.Key and SysConf.Key like "View.%");
-*/
+
 update UserConf
 set Value = (select SysConf.DefaultValue from SysConf where UserConf.Key=SysConf.Key)
 where exists (select * from SysConf where UserConf.Key=SysConf.Key);
 
+*/
 /*
 select Key, ',Value: ',  Value, ',Type: ', ValueType from UserConf;
 select Key, ',DefaultValue: ', DefaultValue, ',Step: ', Step, ',Upper: ', Upper, ',Lower: ', Lower from SysConf;
@@ -36,11 +37,9 @@ select cast(round(0.3, 2) as int);
 
 select Key, ',Value: ',  cast(Value as float), ',Type: ', ValueType from UserConf;
 */
-/*
 select Key,
 ',DefaultValue: ', cast(DefaultValue as float),
 ',Step: ', cast(Step as float),
 ',Upper: ', cast(Upper as float),
 ',Lower: ', cast(Lower as float) from SysConf;
-*/
 commit;
