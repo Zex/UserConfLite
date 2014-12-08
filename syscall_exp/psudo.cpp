0ef7e0b8d20b494cfdd9ff031e81ef532b3020cb
Zex <top_zlynch@yahoo.com>
2014-12-08 22:50:02 +0800

    writev psudo

--- syscall_exp/psudo.cpp
+++ syscall_exp/psudo.cpp
@@ -1,4 +1,5 @@
 
+#include <unistd.h>
 #include "translate.h"
 
 #include <sys/syscall.h>
@@ -32,8 +33,37 @@ void case1(int argc, char* argv[])
     LOG("Got signal\n")
 }
 
+#include <sys/uio.h>
+
+int case14(int argc, char* argv[])
+{
+    int fd = STDOUT_FILENO, iov_nr = 2;
+    struct iovec iov[iov_nr];
+
+    char* base_0 = "start...";
+    char* base_1 = "end...";
+
+    iov[0].iov_base = base_0;
+    iov[0].iov_len = 9;
+    iov[1].iov_base = base_1;
+    iov[1].iov_len = 7;
+
+    if (0 > writev(fd, iov, iov_nr)); 
+    {
+        LOG_ERR(strerror(errno))
+        return 0;
+    }
+
+//    std::cout << "iovec: "
+//        << "\niov_base: " << (char*)iov.iov_base
+//        << "\niov_len: " << iov.iov_len;
+    std::cout << "\n******************************************\n";
+    return 0;
+}
+
 int main(int argc, char* argv[])
 {
-    case1(argc, argv);
+//    case1(argc, argv);
+    case14(argc, argv);
     return 0;
 }
